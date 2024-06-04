d12eea5447184d736da4847c745ffe80567666ec
Camilo López <camiloj.lopez79@gmail.com>
2024-06-04 13:19:33 -0400

    Cambios en el Store

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,5 +1,6 @@
 // React Native
 import {
+  Pressable,
   Text,
   View
 } from 'react-native';
@@ -11,11 +12,33 @@ import { styles } from '../../../config';
 export const ProfileScreen = () => {
   const name = useProfileStore( state => state.name );
   const email = useProfileStore( state => state.email );
+  const changeProfile = useProfileStore( state => state.changeProfile );
 
   return (
     <View style={ styles.container } >
       <Text style={ styles.title }>{ name }</Text>
       <Text style={ styles.title }>{ email }</Text>
+
+      <Pressable
+        style={ styles.primaryButton }
+        onPress={ () => useProfileStore.setState({ name: 'Camilo López' }) }
+      >
+        <Text>Cambiar Nombre</Text>
+      </Pressable>
+
+      <Pressable
+        style={ styles.primaryButton }
+        onPress={ () => useProfileStore.setState({ email: 'c.lopez@google.com' }) }
+      >
+        <Text>Cambiar Email</Text>
+      </Pressable>
+
+      <Pressable
+        style={ styles.primaryButton }
+        onPress={ () => changeProfile( 'John Doe', 'j.doe@google.com' ) }
+      >
+        <Text>Regresar a John</Text>
+      </Pressable>
     </View>
   );
 }
